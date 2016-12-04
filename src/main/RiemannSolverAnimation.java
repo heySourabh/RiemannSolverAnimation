a94c07bc9e14ca1913ca62af2a09e67e492917e6
Sourabh Bhat <heySourabh@gmail.com>
2016-12-04 20:42:06 +0530

    Code for base window

---
+++ src/main/RiemannSolverAnimation.java
@@ -0,0 +1,15 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RiemannSolverAnimation extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
