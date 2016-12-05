f17e88aba024916a3d0371caa7cdedd70224c536
Sourabh Bhat <heySourabh@gmail.com>
2016-12-06 01:19:25 +0530

    Added a dialog box for getting the list of wave speeds. The entered waves are parsed and only the ones in range [-1.5, 1.5] are filtered.

--- src/main/RiemannSolverAnimation.java
+++ src/main/RiemannSolverAnimation.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
@@ -12,6 +14,7 @@ import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
@@ -34,18 +37,25 @@ public class RiemannSolverAnimation extends Application {
     final double BOTTOM_OFFSET = 50;
     final double TOP_OFFSET = 10;
     final double STOP_TIME = 5.0;
-    final static double[] dx_dt = {-1.5, -1, -0.5, 0.5, 1.5};
+    static double[] dx_dt = {-0.25, -0.5, 1.0, 1.5};
 
     boolean playing = true;
 
     public static void main(String[] args) {
-        Arrays.sort(dx_dt);
         System.out.println(Arrays.toString(dx_dt));
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        String dx_dt_list = Arrays.toString(dx_dt);
+        TextInputDialog get_dx_dt = new TextInputDialog(dx_dt_list);
+        get_dx_dt.setTitle("slope: dx/dt");
+        get_dx_dt.setHeaderText("Enter list of wave speeds dx / dt within range [-1.5 to 1.5]");
+        dx_dt_list = get_dx_dt.showAndWait().orElse(dx_dt_list);
+        dx_dt = parseToArray(dx_dt_list);
+        Arrays.sort(dx_dt);
+
         Group root = new Group();
         Scene scene = new Scene(root, WIDTH, HEIGHT);
         primaryStage.setScene(scene);
@@ -138,11 +148,11 @@ public class RiemannSolverAnimation extends Application {
 
         return wavesGroup;
     }
-    
+
     private Group createDistribution(double time) {
         double timeRatio = time / STOP_TIME;
         double xZero = WIDTH / 2;
-        double yZero = HEIGHT -  BOTTOM_OFFSET;
+        double yZero = HEIGHT - BOTTOM_OFFSET;
         double topLimit = HEIGHT / 2.0 - 50;
         double maxHeight = HEIGHT / 2.0 - 2.0 * BOTTOM_OFFSET - 2.0 * TOP_OFFSET - 50;
         double yTime = yZero - timeRatio * maxHeight;
@@ -193,17 +203,17 @@ public class RiemannSolverAnimation extends Application {
 
     private Group createAxis(int id, String xLabel, String yLabel) {
         double xZero = WIDTH / 2;
-        double yZero = id == 1 ? HEIGHT / 2.0 - 2.0 * BOTTOM_OFFSET : HEIGHT -  BOTTOM_OFFSET;
+        double yZero = id == 1 ? HEIGHT / 2.0 - 2.0 * BOTTOM_OFFSET : HEIGHT - BOTTOM_OFFSET;
         double topLimit = id == 1 ? TOP_OFFSET : HEIGHT / 2.0 - 50;
         Path axisX = createArrow(new Point2D(SIDE_OFFSET, yZero), new Point2D(xZero * 2 - SIDE_OFFSET, yZero), 20);
         axisX.setStrokeWidth(2.0);
         Path axisY = createArrow(new Point2D(xZero, yZero), new Point2D(xZero, topLimit), 20);
         axisY.setStrokeWidth(2.0);
-        
+
         Font labelFont = Font.font("serif", FontPosture.ITALIC, 20);
         Text xLabelText = new Text(WIDTH - 80, yZero + 25, xLabel);
         xLabelText.setFont(labelFont);
-        
+
         Text yLabelText = new Text(xZero + 15, topLimit + 25, yLabel);
         yLabelText.setFont(labelFont);
 
@@ -229,5 +239,21 @@ public class RiemannSolverAnimation extends Application {
         arrow.setStrokeLineCap(StrokeLineCap.BUTT);
 
         return arrow;
+    }
+
+    private double[] parseToArray(String dx_dt_list) {
+        String csv = dx_dt_list.trim().substring(1, dx_dt_list.length() - 1);
+        List<Double> list = Arrays.stream(csv.split(","))
+                .map(s -> Double.parseDouble(s.trim()))
+                .filter(value -> value >= -1.51 && value <= 1.51)
+                .collect(Collectors.toList());
+        double[] double_list = new double[list.size()];
+        for (int i = 0; i < double_list.length; i++) {
+            double_list[i] = list.get(i);
+
+        }
+        System.out.println(Arrays.toString(double_list));
+        
+        return double_list;
     }
 }
